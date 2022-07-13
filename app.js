76329b14dc1a002a1dbf15036fd8d84241b8f2d1
Shin <shg7271@gmail.com>
2022-07-13 22:35:44 +0900

    Learn: canvas path

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("hello");
+const canvas = document.querySelector("canvas");
+
+// context: 붓
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.rect(50, 50, 100, 100); // 선 만들기
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+ctx.beginPath(); // 새로운 경로 생성, beginPath()를 실행하면 이전 경로와 단절된다.
+ctx.rect(350, 350, 100, 100);
+ctx.fillStyle = "red";
+ctx.fill();
