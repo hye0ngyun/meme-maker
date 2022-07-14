bedb63d28c5048a3a5826154800d12d47000a44e
Shin <shg7271@gmail.com>
2022-07-14 22:47:39 +0900

    Learn: js canvas drawing line

--- app.js
+++ app.js
@@ -6,10 +6,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.fill();
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.stroke();
+const colors = [
+  "#ffcccc",
+  "#ff9f1a",
+  "#7d5fff",
+  "#32ff7e",
+  "#c56cf0",
+  "#3d3d3d",
+];
+let offsetX = 0;
+let offsetY = 0;
+ctx.lineWidth = 2;
+canvas.addEventListener("mousemove", (e) => {
+  const color = colors[Math.round(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(offsetX, offsetY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+});
+canvas.addEventListener("click", (e) => {
+  offsetX = e.offsetX;
+  offsetY = e.offsetY;
+});
